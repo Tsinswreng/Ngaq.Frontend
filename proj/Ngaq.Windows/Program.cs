122c7da55ce4a2cb7a1325442b382c0ba675009d
Tsinswreng <Tsinswreng@gmail.com>
2025-06-15 11:40:26 +0800

    啓動旹檢DB初始化否

--- proj/Ngaq.Windows/Program.cs
+++ proj/Ngaq.Windows/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Microsoft.Extensions.DependencyInjection;
+using Ngaq.Core.Infra;
 using Ngaq.Local;
+using Ngaq.Local.Sql;
 using Ngaq.Ui;
 using Ngaq.Ui.Views.Word.Query;
 using Ngaq.Ui.Views.Word.WordManage.AddWord;
@@ -9,11 +11,33 @@ namespace Ngaq.Windows;
 
 sealed class Program
 {
+
+	static str GetCfgFilePath(string[] args){
+		var CfgFilePath = "";
+		if(args.Length > 0){
+			CfgFilePath = args[0];
+		}else{
+#if DEBUG
+			CfgFilePath = "Ngaq.dev.yml";
+#else
+			CfgFilePath = "Ngaq.yml";
+#endif
+		}
+		return CfgFilePath;
+	}
+
 	// Initialization code. Don't use any Avalonia, third-party APIs or any
 	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
 	// yet and stuff might break.
 	[STAThread]
 	public static void Main(string[] args){
+		try{
+			AppCfg.Inst = AppCfgParser.Inst.FromYaml(GetCfgFilePath(args));
+		}
+		catch (System.Exception e){
+			System.Console.Error.WriteLine("Failed to load config file: "+e);
+		}
+
 		var svc = new ServiceCollection();
 		DiLocal.SetUpLocal(svc);
 		//new Setup().ConfigureServices(services);
@@ -34,7 +58,11 @@ sealed class Program
 
 		var servicesProvider = svc.BuildServiceProvider();
 		BuildAvaloniaApp()
-			.AfterSetup(e=>App.ConfigureServices(servicesProvider))
+			.AfterSetup(e=>{
+				App.ConfigureServices(servicesProvider);
+				var DbIniter = App.GetSvc<DbIniter>();
+				_ = DbIniter.Init(default).Result;
+			})
 			.StartWithClassicDesktopLifetime(args)
 		;
 	}
