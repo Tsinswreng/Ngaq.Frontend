f0abb4ec674029b2d2e2c10eae2ccbdd81a67ae8
Tsinswreng <Tsinswreng@gmail.com>
2026-01-22 16:08:27 +0800

    治DfltMainColor不效

--- proj/Ngaq.Ui/UiCfg.cs
+++ proj/Ngaq.Ui/UiCfg.cs
@@ -12,29 +12,34 @@ public  partial class UiCfg{
 
 	public UiCfg(){
 		BaseFontSize = ItemsClientCfg.BaseFontSize.GetFrom(AppCfg.Inst);
-		var got = ResolveThemeBrush();
-		if(got != null){
-			MainColor = got;
-		}
+		MainColor = ResolveThemeBrush();
 	}
 
 
 	public f64 BaseFontSize { get; set; } = 16.0;
 	public f64 WindowWidth {get;set;}= 400;
 	public f64 WindowHeight {get;set;}= 700;
-	public IBrush MainColor {get;set;}= Brushes.Blue;
+	public IBrush? MainColor {get;set;}= DfltMainColor;
 	public IBrush ForegroundColor {get;set;}= Brushes.White;
 	public IBrush BackgroundColor {get;set;}= Brushes.Black;
+	public static IBrush DfltMainColor = new SolidColorBrush(Color.FromArgb(0xFF, 0x00, 0x78, 0xD4));
 
-	protected IBrush? ResolveThemeBrush(){
+	protected IBrush ResolveThemeBrush(){
 		var app = Application.Current;
-		if (app == null) return null;
-		// 嘗試幾個常見的主題資源鍵
+		if (app == null){
+			return DfltMainColor;
+		}
+
+		// 嘗試主題資源鍵 (Avalonia first, then Windows)
 		var keys = new[] {
+			"ThemeAccentBrush",
+			"ThemeAccentBrush2",
+			"ThemeAccentBrush3",
+			"ThemeAccentBrush4",
+			"AccentBrush",
 			"SystemControlHighlightAccentBrush",
 			"SystemControlForegroundBaseHighBrush",
 			"SystemAccentColorBrush",
-			"AccentBrush",
 			"AccentColorBrush",
 		};
 		foreach(var k in keys){
@@ -42,6 +47,6 @@ public  partial class UiCfg{
 				return b;
 			}
 		}
-		return null;
+		return DfltMainColor;
 	}
 }
