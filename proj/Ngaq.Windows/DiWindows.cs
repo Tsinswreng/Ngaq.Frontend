fd864368980e5f29a7c2c2c741ea939357701e05
Tsinswreng <Tsinswreng@gmail.com>
2026-02-25 23:09:03 +0800

    Di clipboard

--- proj/Ngaq.Windows/DiWindows.cs
+++ proj/Ngaq.Windows/DiWindows.cs
@@ -1,12 +1,15 @@
 using Microsoft.Extensions.DependencyInjection;
+using Ngaq.Core.Frontend.Clipboard;
 using Ngaq.Core.Shared.Audio;
 using Ngaq.Windows.Domains.Audio;
+using Ngaq.Windows.Domains.Clipboard;
 
 namespace Ngaq.Windows;
 
 public static class DiWindows{
 	public static IServiceCollection SetupWindows(this IServiceCollection z){
 		z.AddSingleton<IAudioPlayer, NAudioPlayer>();
+		z.AddSingleton<ISvcClipboard, SvcClipboard>();
 		return z;
 	}
 }
