5f0d7b3d84fdc5164ac48828067ec0da1548da32
Tsinswreng <Tsinswreng@gmail.com>
2026-03-21 17:06:45 +0800

    ai:StudyPlan

--- proj/Ngaq.Ui/Views/Word/WordManage/StudyPlan/ViewStudyPlan.cs
+++ proj/Ngaq.Ui/Views/Word/WordManage/StudyPlan/ViewStudyPlan.cs
@@ -1,10 +1,19 @@
-namespace Ngaq.Ui.Views.Word.WordManage.StudyPlan;
+﻿namespace Ngaq.Ui.Views.Word.WordManage.StudyPlan;
 
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Layout;
+using Avalonia.Media;
 using Ngaq.Ui;
+using Ngaq.Ui.Components.KvMap.JsonMap;
 using Ngaq.Ui.Infra;
 using Ngaq.Ui.Infra.I18n;
+using Tsinswreng.AvlnTools.Dsl;
 using Tsinswreng.AvlnTools.Tools;
 using Ctx = VmStudyPlan;
+
 public partial class ViewStudyPlan
 	:AppViewBase
 {
@@ -31,9 +40,252 @@ public partial class ViewStudyPlan
 
 	AutoGrid Root = new(IsRow: true);
 	protected nil Render(){
+		this.Content = Root.Grid;
+		Root.Grid.RowDefinitions.AddRange([
+			RowDef(1, GUT.Auto),
+			RowDef(8, GUT.Star),
+			RowDef(1, GUT.Auto),
+			RowDef(1, GUT.Auto),
+		]);
+
+		Root
+		.AddInit(MkTopBar(), o=>{})
+		.AddInit(MkBody(), o=>{})
+		.AddInit(MkErrBar(), o=>{})
+		.AddInit(MkBottomBar(), o=>{})
+		;
 
 		return NIL;
 	}
 
+	Control MkTopBar(){
+		var g = new AutoGrid(IsRow:false);
+		g.Grid.Margin = new Thickness(8, 8, 8, 4);
+		g.Grid.ColumnDefinitions.AddRange([
+			ColDef(1, GUT.Auto),
+			ColDef(1, GUT.Auto),
+			ColDef(1, GUT.Auto),
+			ColDef(1, GUT.Auto),
+			ColDef(1, GUT.Auto),
+			ColDef(1, GUT.Star),
+			ColDef(2, GUT.Star),
+		]);
+		g
+		.AddInit(MkBtn("New"), o=>{ o.Click += (s,e)=>Ctx?.NewPlan(); })
+		.AddInit(MkBtn("Clone"), o=>{ o.Click += (s,e)=>Ctx?.CloneSelected(); })
+		.AddInit(MkBtn("Delete"), o=>{ o.Click += (s,e)=>Ctx?.DeleteSelected(); })
+		.AddInit(MkBtn("Set Current"), o=>{ o.Click += (s,e)=>Ctx?.SetCurrentSelected(); })
+		.AddInit(MkBtn("Save Draft"), o=>{ o.Click += (s,e)=>Ctx?.SaveFormToSelected(); })
+		.AddInit(MkSearch(), o=>{});
+		return g.Grid;
+	}
 
+	Control MkSearch(){
+		var tb = new TextBox();
+		tb.Watermark = "Search by name or id";
+		tb.Bind(tb.PropText, CBE.Mk<Ctx>(x=>x.SearchText, Mode:BindingMode.TwoWay));
+		return tb;
+	}
+
+	Control MkBody(){
+		var g = new AutoGrid(IsRow:false);
+		g.Grid.Margin = new Thickness(8, 2, 8, 2);
+		g.Grid.ColumnDefinitions.AddRange([
+			ColDef(3, GUT.Star),
+			ColDef(7, GUT.Star),
+		]);
+		g
+		.AddInit(MkPlanList(), o=>{})
+		.AddInit(MkDetailPanel(), o=>{})
+		;
+		return g.Grid;
+	}
+
+	Control MkPlanList(){
+		var b = new Border{
+			BorderBrush = Brushes.Gray,
+			BorderThickness = new Thickness(1),
+			Padding = new Thickness(6),
+		};
+		var list = new ListBox();
+		list.Bind(list.PropItemsSource, CBE.Mk<Ctx>(x=>x.FilteredPlans));
+		list.Bind(list.PropSelectedItem, CBE.Mk<Ctx>(x=>x.SelectedPlan, Mode:BindingMode.TwoWay));
+		list.SetItemTemplate<Ctx.PlanDraft>((item, ns)=>{
+			var txt = new TextBlock();
+			txt.Bind(txt.PropText, CBE.Mk<Ctx.PlanDraft>(x=>x.DisplayName));
+			return txt;
+		});
+		b.Child = list;
+		return b;
+	}
+
+	Control MkDetailPanel(){
+		var tab = new TabControl();
+		tab.Items.AddInit(new TabItem(), o=>{
+			o.Header = "Form";
+			o.Content = MkFormTab();
+		}).AddInit(new TabItem(), o=>{
+			o.Header = "Weight Arg";
+			o.Content = MkWeightArgTab();
+		}).AddInit(new TabItem(), o=>{
+			o.Header = "JSON";
+			o.Content = MkJsonTab();
+		});
+		return tab;
+	}
+
+	Control MkFormTab(){
+		var sv = new ScrollViewer();
+		var sp = new StackPanel{
+			Spacing = 8,
+			Margin = new Thickness(8),
+		};
+		sv.Content = sp;
+
+		sp
+		.AddInit(MkReadOnlyRow("PlanId", CBE.Mk<Ctx>(x=>x.PlanIdText, Mode:BindingMode.TwoWay)))
+		.AddInit(MkInputRow("UniqName", CBE.Mk<Ctx>(x=>x.UniqName, Mode:BindingMode.TwoWay)))
+		.AddInit(MkInputRow("Descr", CBE.Mk<Ctx>(x=>x.Descr, Mode:BindingMode.TwoWay)))
+		.AddInit(MkComboRow("PreFilter", CBE.Mk<Ctx>(x=>x.PreFilterOptions), CBE.Mk<Ctx>(x=>x.SelectedPreFilterId, Mode:BindingMode.TwoWay)))
+		.AddInit(MkComboRow("WeightCalculator", CBE.Mk<Ctx>(x=>x.WeightCalculatorOptions), CBE.Mk<Ctx>(x=>x.SelectedWeightCalculatorId, Mode:BindingMode.TwoWay)))
+		.AddInit(MkComboRow("WeightArg", CBE.Mk<Ctx>(x=>x.WeightArgOptions), CBE.Mk<Ctx>(x=>x.SelectedWeightArgId, Mode:BindingMode.TwoWay)))
+		.AddInit(MkJsonTextRow("PreFilter Json", CBE.Mk<Ctx>(x=>x.PreFilterJson, Mode:BindingMode.TwoWay), 120))
+		.AddInit(MkJsonTextRow("WeightCalculator Json", CBE.Mk<Ctx>(x=>x.WeightCalculatorJson, Mode:BindingMode.TwoWay), 120))
+		;
+
+		return sv;
+	}
+
+	Control MkWeightArgTab(){
+		var g = new AutoGrid(IsRow:true);
+		g.Grid.RowDefinitions.AddRange([
+			RowDef(1, GUT.Auto),
+			RowDef(9, GUT.Star),
+		]);
+		g.AddInit(new TextBlock(), o=>{
+			o.Margin = new Thickness(8,8,8,4);
+			o.Text = "Edit WeightArg via UiJsonMap. Save/Sync will collect these values.";
+			o.Foreground = Brushes.LightGray;
+		});
+		g.AddInit(new ViewUiJsonMap(){
+			Ctx = Ctx?.WeightArgMapVm
+		}, o=>{
+			o.Margin = new Thickness(8,0,8,8);
+		});
+		return g.Grid;
+	}
+
+	Control MkJsonTab(){
+		var g = new AutoGrid(IsRow:true);
+		g.Grid.RowDefinitions.AddRange([
+			RowDef(1, GUT.Auto),
+			RowDef(9, GUT.Star),
+		]);
+		g.AddInit(MkJsonOps(), o=>{});
+		g.AddInit(new TextBox(), o=>{
+			o.Margin = new Thickness(8,4,8,8);
+			o.AcceptsReturn = true;
+			o.TextWrapping = TextWrapping.Wrap;
+			o.SetValue(ScrollViewer.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Auto);
+			o.Bind(o.PropText, CBE.Mk<Ctx>(x=>x.JsonText, Mode:BindingMode.TwoWay));
+		});
+		return g.Grid;
+	}
+
+	Control MkJsonOps(){
+		var g = new AutoGrid(IsRow:false);
+		g.Grid.Margin = new Thickness(8,8,8,2);
+		g.Grid.ColumnDefinitions.AddRange([
+			ColDef(1, GUT.Star),
+			ColDef(1, GUT.Star),
+		]);
+		g
+		.AddInit(MkBtn("Sync Form -> Json"), o=>{ o.Click += (s,e)=>Ctx?.SyncJsonFromForm(); })
+		.AddInit(MkBtn("Apply Json -> Form"), o=>{ o.Click += (s,e)=>Ctx?.ApplyJsonToForm(); })
+		;
+		return g.Grid;
+	}
+
+	Control MkErrBar(){
+		var b = new Border{
+			Background = new SolidColorBrush(Color.FromArgb(70, 180, 20, 20)),
+			Padding = new Thickness(8, 4),
+			Margin = new Thickness(8, 2, 8, 2),
+			IsVisible = false,
+		};
+		b.Bind(IsVisibleProperty, CBE.Mk<Ctx>(x=>x.HasError, Mode:BindingMode.OneWay));
+		var txt = new TextBlock{ Foreground = Brushes.White };
+		txt.Bind(txt.PropText, CBE.Mk<Ctx>(x=>x.LastError, Mode:BindingMode.OneWay));
+		b.Child = txt;
+		return b;
+	}
+
+	Control MkBottomBar(){
+		var g = new AutoGrid(IsRow:false);
+		g.Grid.Margin = new Thickness(8, 2, 8, 8);
+		g.Grid.ColumnDefinitions.AddRange([
+			ColDef(1, GUT.Star),
+			ColDef(1, GUT.Star),
+			ColDef(1, GUT.Star),
+			ColDef(1, GUT.Auto),
+		]);
+		g
+		.AddInit(MkBtn("Save Form"), o=>{ o.Click += (s,e)=>Ctx?.SaveFormToSelected(); })
+		.AddInit(MkBtn("Sync Form -> Json"), o=>{ o.Click += (s,e)=>Ctx?.SyncJsonFromForm(); })
+		.AddInit(MkBtn("Apply Json -> Form"), o=>{ o.Click += (s,e)=>Ctx?.ApplyJsonToForm(); })
+		.AddInit(new TextBlock(), o=>{
+			o.VerticalAlignment = VAlign.Center;
+			o.Text = "Dirty";
+			o.Foreground = Brushes.Orange;
+			o.Bind(IsVisibleProperty, CBE.Mk<Ctx>(x=>x.IsDirty, Mode:BindingMode.OneWay));
+		})
+		;
+		return g.Grid;
+	}
+
+	Control MkInputRow(str label, IBinding binding){
+		var sp = new StackPanel{ Orientation = Orientation.Vertical, Spacing = 2 };
+		sp.Children.Add(new TextBlock{ Text = label });
+		var tb = new TextBox();
+		tb.Bind(tb.PropText, binding);
+		sp.Children.Add(tb);
+		return sp;
+	}
+
+	Control MkReadOnlyRow(str label, IBinding binding){
+		var sp = new StackPanel{ Orientation = Orientation.Vertical, Spacing = 2 };
+		sp.Children.Add(new TextBlock{ Text = label });
+		var tb = new TextBox{ IsReadOnly = true };
+		tb.Bind(tb.PropText, binding);
+		sp.Children.Add(tb);
+		return sp;
+	}
+
+	Control MkComboRow(str label, IBinding itemsBinding, IBinding selectedBinding){
+		var sp = new StackPanel{ Orientation = Orientation.Vertical, Spacing = 2 };
+		sp.Children.Add(new TextBlock{ Text = label });
+		var cb = new ComboBox();
+		cb.Bind(cb.PropItemsSource, itemsBinding);
+		cb.Bind(cb.PropSelectedItem, selectedBinding);
+		sp.Children.Add(cb);
+		return sp;
+	}
+
+	Control MkJsonTextRow(str label, IBinding binding, double height){
+		var sp = new StackPanel{ Orientation = Orientation.Vertical, Spacing = 2 };
+		sp.Children.Add(new TextBlock{ Text = label });
+		var tb = new TextBox{
+			Height = height,
+			AcceptsReturn = true,
+			TextWrapping = TextWrapping.Wrap,
+		};
+		tb.SetValue(ScrollViewer.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Auto);
+		tb.Bind(tb.PropText, binding);
+		sp.Children.Add(tb);
+		return sp;
+	}
+
+	Button MkBtn(str text){
+		return new Button{ Content = text, Margin = new Thickness(0,0,8,0) };
+	}
 }
--- proj/Ngaq.Ui/Views/Word/WordManage/StudyPlan/VmStudyPlan.cs
+++ proj/Ngaq.Ui/Views/Word/WordManage/StudyPlan/VmStudyPlan.cs
@@ -1,11 +1,19 @@
-namespace Ngaq.Ui.Views.Word.WordManage.StudyPlan;
+﻿namespace Ngaq.Ui.Views.Word.WordManage.StudyPlan;
 using System.Collections.ObjectModel;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Ngaq.Core.Tools.JsonMap;
+using Ngaq.Ui.Components.KvMap.JsonMap;
 using Ngaq.Ui.Infra;
+using Tsinswreng.CsTools;
 
 using Ctx = VmStudyPlan;
 public partial class VmStudyPlan: ViewModelBase, IMk<Ctx>{
-	//蔿從構造函數依賴注入、故以靜態工廠代無參構造器
-	protected VmStudyPlan(){}
+	protected VmStudyPlan(){
+		InitSamples();
+		ApplyFilter();
+		SelectedPlan = FilteredPlans.FirstOrDefault();
+	}
 	public static Ctx Mk(){
 		return new Ctx();
 	}
@@ -20,11 +28,528 @@ public partial class VmStudyPlan: ViewModelBase, IMk<Ctx>{
 		#endif
 	}
 
+	public ObservableCollection<PlanDraft> Plans{
+		get{return field;}
+		set{SetProperty(ref field, value);}
+	}=[];
 
-	public str YYY{
+	public ObservableCollection<PlanDraft> FilteredPlans{
+		get{return field;}
+		set{SetProperty(ref field, value);}
+	}=[];
+
+	public PlanDraft? SelectedPlan{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				LoadSelectedToForm();
+			}
+		}
+	}
+
+	public str SearchText{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				ApplyFilter();
+			}
+		}
+	}="";
+
+	public str CurPlanId{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				foreach(var plan in Plans){
+					plan.RefreshDisplay(CurPlanId);
+				}
+			}
+		}
+	}="";
+
+	public str PlanIdText{
 		get{return field;}
 		set{SetProperty(ref field, value);}
 	}="";
 
+	public str UniqName{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="";
 
+	public str Descr{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="";
+
+	public str? SelectedPreFilterId{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="";
+
+	public str? SelectedWeightCalculatorId{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="";
+
+	public str? SelectedWeightArgId{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="";
+
+	public ObservableCollection<str> PreFilterOptions{get;set;}=[
+		"pf-all",
+		"pf-lang-en",
+		"pf-lang-ja",
+	];
+
+	public ObservableCollection<str> WeightCalculatorOptions{get;set;}=[
+		"wcal-default",
+		"wcal-js-v1",
+	];
+
+	public ObservableCollection<str> WeightArgOptions{get;set;}=[
+		"warg-default",
+		"warg-hardcore",
+	];
+
+	public str PreFilterJson{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="{\n  \"lang\": [\"en\"]\n}";
+
+	public str WeightCalculatorJson{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="{\n  \"type\": \"js\",\n  \"entry\": \"calc\"\n}";
+
+	public VmUiJsonMap WeightArgMapVm{
+		get{return field;}
+		set{SetProperty(ref field, value);}
+	}=VmUiJsonMap.Mk();
+
+	public str JsonText{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				Touch();
+			}
+		}
+	}="";
+
+	public str LastError{
+		get{return field;}
+		set{
+			if(SetProperty(ref field, value)){
+				OnPropertyChanged(nameof(HasError));
+			}
+		}
+	}="";
+
+	public bool HasError => !str.IsNullOrWhiteSpace(LastError);
+
+	public bool IsDirty{
+		get{return field;}
+		set{SetProperty(ref field, value);}
+	}
+
+	bool _isHydrating = false;
+	void Touch(){
+		if(_isHydrating){
+			return;
+		}
+		IsDirty = true;
+	}
+
+	public nil NewPlan(){
+		var plan = new PlanDraft{
+			Id = Guid.NewGuid().ToString("N"),
+			UniqName = "new-plan",
+			Descr = "",
+			PreFilterId = PreFilterOptions.FirstOrDefault()??"",
+			WeightCalculatorId = WeightCalculatorOptions.FirstOrDefault()??"",
+			WeightArgId = WeightArgOptions.FirstOrDefault()??"",
+			PreFilterJson = "{\n  \"lang\": [\"en\"]\n}",
+			WeightCalculatorJson = "{\n  \"type\": \"js\",\n  \"entry\": \"calc\"\n}",
+			WeightArgRaw = MkDefaultWeightArgDict(),
+		};
+		Plans.Add(plan);
+		ApplyFilter();
+		SelectedPlan = plan;
+		IsDirty = true;
+		return NIL;
+	}
+
+	public nil CloneSelected(){
+		if(SelectedPlan is null){
+			ShowMsg("No selected plan.");
+			return NIL;
+		}
+		var neo = SelectedPlan.DeepClone();
+		neo.Id = Guid.NewGuid().ToString("N");
+		neo.UniqName = $"{SelectedPlan.UniqName}-copy";
+		Plans.Add(neo);
+		ApplyFilter();
+		SelectedPlan = neo;
+		IsDirty = true;
+		return NIL;
+	}
+
+	public nil DeleteSelected(){
+		if(SelectedPlan is null){
+			ShowMsg("No selected plan.");
+			return NIL;
+		}
+		var id = SelectedPlan.Id;
+		Plans.Remove(SelectedPlan);
+		ApplyFilter();
+		SelectedPlan = FilteredPlans.FirstOrDefault();
+		if(CurPlanId == id){
+			CurPlanId = "";
+		}
+		IsDirty = true;
+		return NIL;
+	}
+
+	public nil SetCurrentSelected(){
+		if(SelectedPlan is null){
+			ShowMsg("No selected plan.");
+			return NIL;
+		}
+		CurPlanId = SelectedPlan.Id;
+		return NIL;
+	}
+
+	public nil SaveFormToSelected(){
+		if(SelectedPlan is null){
+			ShowMsg("No selected plan.");
+			return NIL;
+		}
+		if(!TryBuildDtoFromForm(out var dto, out var err)){
+			LastError = err;
+			ShowMsg(err);
+			return NIL;
+		}
+		LastError = "";
+		SelectedPlan.FromDto(dto!);
+		SelectedPlan.RefreshDisplay(CurPlanId);
+		SyncJsonFromForm();
+		IsDirty = false;
+		ShowMsg("Saved in draft.");
+		return NIL;
+	}
+
+	public nil SyncJsonFromForm(){
+		if(!TryBuildDtoFromForm(out var dto, out var err)){
+			LastError = err;
+			return NIL;
+		}
+		LastError = "";
+		JsonText = JsonSerializer.Serialize(
+			dto,
+			new JsonSerializerOptions{
+				WriteIndented = true,
+				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+			}
+		);
+		return NIL;
+	}
+
+	public nil ApplyJsonToForm(){
+		try{
+			var dto = JsonSerializer.Deserialize<PlanDto>(JsonText);
+			if(dto is null){
+				LastError = "Json parse failed.";
+				return NIL;
+			}
+			FillFormFromDto(dto);
+			LastError = "";
+			IsDirty = true;
+		}catch(Exception ex){
+			LastError = ex.Message;
+		}
+		return NIL;
+	}
+
+	void LoadSelectedToForm(){
+		if(SelectedPlan is null){
+			return;
+		}
+		_isHydrating = true;
+		try{
+			FillFormFromDto(SelectedPlan.ToDto());
+			SyncJsonFromForm();
+			IsDirty = false;
+			LastError = "";
+		}finally{
+			_isHydrating = false;
+		}
+	}
+
+	void FillFormFromDto(PlanDto dto){
+		PlanIdText = dto.Id ?? "";
+		UniqName = dto.UniqName ?? "";
+		Descr = dto.Descr ?? "";
+		SelectedPreFilterId = dto.PreFilterId ?? "";
+		SelectedWeightCalculatorId = dto.WeightCalculatorId ?? "";
+		SelectedWeightArgId = dto.WeightArgId ?? "";
+		PreFilterJson = dto.PreFilterJson ?? "{}";
+		WeightCalculatorJson = dto.WeightCalculatorJson ?? "{}";
+		BindWeightArgMap(dto.WeightArgRaw ?? MkDefaultWeightArgDict());
+	}
+
+	bool TryBuildDtoFromForm(out PlanDto? dto, out str err){
+		dto = null;
+		err = "";
+		if(str.IsNullOrWhiteSpace(PlanIdText)){
+			err = "PlanId is required.";
+			return false;
+		}
+		if(str.IsNullOrWhiteSpace(UniqName)){
+			err = "UniqName is required.";
+			return false;
+		}
+
+		WeightArgMapVm.UpdData();
+		var weightArgRaw = ExtractWeightArgRaw();
+
+		dto = new PlanDto{
+			Id = PlanIdText.Trim(),
+			UniqName = UniqName.Trim(),
+			Descr = Descr,
+			PreFilterId = SelectedPreFilterId ?? "",
+			WeightCalculatorId = SelectedWeightCalculatorId ?? "",
+			WeightArgId = SelectedWeightArgId ?? "",
+			PreFilterJson = PreFilterJson,
+			WeightCalculatorJson = WeightCalculatorJson,
+			WeightArgRaw = weightArgRaw,
+		};
+		return true;
+	}
+
+	Dictionary<str, object?> ExtractWeightArgRaw(){
+		var node = WeightArgMapVm.UiJsonMap?.Raw;
+		if(node?.ValueObj is IDictionary<str, object?> d){
+			return d.ToDictionary(x=>x.Key, x=>x.Value);
+		}
+		return MkDefaultWeightArgDict();
+	}
+
+	void BindWeightArgMap(Dictionary<str, object?> raw){
+		var map = new UiJsonMap(){
+			Raw = new JsonNode(raw),
+		};
+		var pathMap = new Dictionary<str, IUiJsonMapItem>();
+
+		pathMap["DfltAddBonus"] = new UiJsonMapItem(map, "DfltAddBonus"){
+			JsonValueType = EJsonValueType.Number,
+			DisplayName = UiText.FromRawText("Default Add Bonus"),
+			Descr = UiText.FromRawText("Base bonus for add event"),
+		};
+		pathMap["DebuffNumerator"] = new UiJsonMapItem(map, "DebuffNumerator"){
+			JsonValueType = EJsonValueType.Number,
+			DisplayName = UiText.FromRawText("Debuff Numerator"),
+			Descr = UiText.FromRawText("Bigger value means slower decay"),
+		};
+		pathMap["AddCnt_Bonus[0]"] = new UiJsonMapItem(map, "AddCnt_Bonus[0]"){
+			JsonValueType = EJsonValueType.Number,
+			DisplayName = UiText.FromRawText("AddCnt Bonus #1"),
+			Descr = UiText.FromRawText("Bonus when add-count reaches level 1"),
+		};
+		pathMap["AddCnt_Bonus[1]"] = new UiJsonMapItem(map, "AddCnt_Bonus[1]"){
+			JsonValueType = EJsonValueType.Number,
+			DisplayName = UiText.FromRawText("AddCnt Bonus #2"),
+			Descr = UiText.FromRawText("Bonus when add-count reaches level 2"),
+		};
+		map.PathToUiMap = pathMap;
+		WeightArgMapVm.FromBo(map);
+	}
+
+	static Dictionary<str, object?> MkDefaultWeightArgDict(){
+		return new Dictionary<str, object?>(){
+			["DfltAddBonus"] = 100d,
+			["DebuffNumerator"] = 36d,
+			["AddCnt_Bonus"] = new List<object?>(){64d, 128d},
+		};
+	}
+
+	void ApplyFilter(){
+		var q = (SearchText??"").Trim();
+		IEnumerable<PlanDraft> seq = Plans;
+		if(!str.IsNullOrWhiteSpace(q)){
+			seq = seq.Where(x=>
+				(x.UniqName?.Contains(q, StringComparison.OrdinalIgnoreCase)??false)
+				|| (x.Id?.Contains(q, StringComparison.OrdinalIgnoreCase)??false)
+			);
+		}
+		FilteredPlans = new ObservableCollection<PlanDraft>(seq);
+		if(SelectedPlan is not null && !FilteredPlans.Contains(SelectedPlan)){
+			SelectedPlan = FilteredPlans.FirstOrDefault();
+		}
+		foreach(var plan in Plans){
+			plan.RefreshDisplay(CurPlanId);
+		}
+	}
+
+	void InitSamples(){
+		var p1 = new PlanDraft{
+			Id = "sp-default",
+			UniqName = "default",
+			Descr = "Default study strategy",
+			PreFilterId = "pf-all",
+			WeightCalculatorId = "wcal-default",
+			WeightArgId = "warg-default",
+			PreFilterJson = "{\n  \"lang\": [\"en\", \"ja\"]\n}",
+			WeightCalculatorJson = "{\n  \"type\": \"builtin\",\n  \"name\": \"default\"\n}",
+			WeightArgRaw = MkDefaultWeightArgDict(),
+		};
+		var p2 = new PlanDraft{
+			Id = "sp-hardcore",
+			UniqName = "hardcore",
+			Descr = "Faster decay with stricter prefilter",
+			PreFilterId = "pf-lang-ja",
+			WeightCalculatorId = "wcal-js-v1",
+			WeightArgId = "warg-hardcore",
+			PreFilterJson = "{\n  \"lang\": [\"ja\"],\n  \"tag\": [\"jlpt\"]\n}",
+			WeightCalculatorJson = "{\n  \"type\": \"js\",\n  \"entry\": \"calc_hardcore\"\n}",
+			WeightArgRaw = new Dictionary<str, object?>(){
+				["DfltAddBonus"] = 80d,
+				["DebuffNumerator"] = 18d,
+				["AddCnt_Bonus"] = new List<object?>(){32d, 64d},
+			},
+		};
+		Plans = [p1, p2];
+		CurPlanId = p1.Id;
+		foreach(var p in Plans){
+			p.RefreshDisplay(CurPlanId);
+		}
+	}
+
+	public class PlanDto{
+		public str? Id{get;set;}
+		public str? UniqName{get;set;}
+		public str? Descr{get;set;}
+		public str? PreFilterId{get;set;}
+		public str? WeightCalculatorId{get;set;}
+		public str? WeightArgId{get;set;}
+		public str? PreFilterJson{get;set;}
+		public str? WeightCalculatorJson{get;set;}
+		public Dictionary<str, object?>? WeightArgRaw{get;set;}
+	}
+
+	public partial class PlanDraft: ViewModelBase{
+		public str Id{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="";
+		public str UniqName{
+			get{return field;}
+			set{
+				if(SetProperty(ref field, value)){
+					UpdateDisplayName();
+				}
+			}
+		}="";
+		public str Descr{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="";
+		public str PreFilterId{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="";
+		public str WeightCalculatorId{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="";
+		public str WeightArgId{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="";
+		public str PreFilterJson{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="{}";
+		public str WeightCalculatorJson{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="{}";
+		public Dictionary<str, object?> WeightArgRaw{get;set;} = [];
+
+		public str DisplayName{
+			get{return field;}
+			set{SetProperty(ref field, value);}
+		}="";
+
+		public void RefreshDisplay(str CurId){
+			UpdateDisplayName(CurId);
+		}
+
+		void UpdateDisplayName(str CurId = ""){
+			var cur = Id == CurId ? "[Current] " : "";
+			DisplayName = $"{cur}{UniqName} ({Id})";
+		}
+
+		public PlanDto ToDto(){
+			return new PlanDto{
+				Id = Id,
+				UniqName = UniqName,
+				Descr = Descr,
+				PreFilterId = PreFilterId,
+				WeightCalculatorId = WeightCalculatorId,
+				WeightArgId = WeightArgId,
+				PreFilterJson = PreFilterJson,
+				WeightCalculatorJson = WeightCalculatorJson,
+				WeightArgRaw = WeightArgRaw.ToDictionary(x=>x.Key, x=>x.Value),
+			};
+		}
+
+		public nil FromDto(PlanDto dto){
+			Id = dto.Id ?? Id;
+			UniqName = dto.UniqName ?? "";
+			Descr = dto.Descr ?? "";
+			PreFilterId = dto.PreFilterId ?? "";
+			WeightCalculatorId = dto.WeightCalculatorId ?? "";
+			WeightArgId = dto.WeightArgId ?? "";
+			PreFilterJson = dto.PreFilterJson ?? "{}";
+			WeightCalculatorJson = dto.WeightCalculatorJson ?? "{}";
+			WeightArgRaw = dto.WeightArgRaw?.ToDictionary(x=>x.Key, x=>x.Value) ?? [];
+			return NIL;
+		}
+
+		public PlanDraft DeepClone(){
+			var dto = ToDto();
+			var neo = new PlanDraft();
+			neo.FromDto(dto);
+			return neo;
+		}
+	}
 }
