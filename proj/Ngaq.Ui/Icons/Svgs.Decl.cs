e119b305a3bf172f872c7343bdf52a7ff789e5c3
Tsinswreng <Tsinswreng@gmail.com>
2026-03-29 23:46:21 +0800

    Svgs 聲明與實現分離

---
+++ proj/Ngaq.Ui/Icons/Svgs.Decl.cs
@@ -0,0 +1,87 @@
+using Tsinswreng.CsCore;
+namespace Ngaq.Ui.Icons;
+[Doc(@$"只存 此應用實際用到的圖標
+定義成方法而非訪問器、方便在分部類中寫部分聲明、聲明與實現分離 㕥作文檔。
+禁止AI閱讀 Svgs.Impl.cs
+")]
+public partial class Svgs{
+	//<iconPacks:PackIconPhosphorIcons Kind="ArrowCircleLeftFill" />
+	public static partial Svg ArrowCircleLeftFill();
+	//<iconPacks:PackIconPhosphorIcons Kind="ArrowCircleRightFill" />
+	public static partial Svg ArrowCircleRightFill();
+
+	//<iconPacks:PackIconPhosphorIcons Kind="PlayCircle" />
+	public static partial Svg PlayCircle();
+	//<iconPacks:PackIconPhosphorIcons Kind="PlayCircleFill" />
+	public static partial Svg PlayCircleFill();
+
+	//<iconPacks:PackIconPhosphorIcons Kind="FloppyDiskBackLight" />
+	public static partial Svg FloppyDiskBackLight();
+
+	//<iconPacks:PackIconPhosphorIcons Kind="FloppyDiskBackFill" />
+	public static partial Svg FloppyDiskBackFill();
+
+	//<iconPacks:PackIconPhosphorIcons Kind="XCircleFill" />
+	public static partial Svg XCircleFill();
+	//<iconPacks:PackIconPhosphorIcons Kind="UserCircleFill" />
+	public static partial Svg UserCircleFill();
+	//<iconPacks:PackIconPhosphorIcons Kind="BookOpenTextFill" />
+	public static partial Svg BookOpenTextFill();
+	//<iconPacks:PackIconPhosphorIcons Kind="BookBookmarkFill" />
+	public static partial Svg BookBookmarkFill();
+
+	//<iconPacks:PackIconPhosphorIcons Kind="GearFill" />
+	public static partial Svg GearFill();
+
+	//<iconPacks:PackIconPhosphorIcons Kind="GearSix" />
+	public static partial Svg GearSix();
+	//<iconPacks:PackIconPhosphorIcons Kind="ArrowClockwiseBold" />
+	public static partial Svg ArrowClockwiseBold();
+
+	//<iconPacks:PackIconLucide Kind="RotateCcw" />
+	public static partial Svg RotateCcw();
+	//<iconPacks:PackIconLucide Kind="RotateCw" />
+	public static partial Svg RotateCw();
+	//<iconPacks:PackIconZondicons Kind="Search" />
+	public static partial Svg Search();
+	//<iconPacks:PackIconModern Kind="Add" />
+	public static partial Svg Add();
+	//<iconPacks:PackIconMaterial Kind="SyncCircle" />
+	public static partial Svg SyncCircle();
+	//<iconPacks:PackIconPhosphorIcons Kind="ChartLineUpFill" />
+	public static partial Svg ChartLineUpFill();
+	//<iconPacks:PackIconForkAwesome Kind="CloudUpload" />
+	public static partial Svg CloudUpload();
+	//<iconPacks:PackIconForkAwesome Kind="CloudDownload" />
+	public static partial Svg CloudDownload();
+	//<iconPacks:PackIconFontisto Kind="Import" />
+	public static partial Svg Import();
+	//<iconPacks:PackIconBoxIcons Kind="SolidFileImport" />
+	public static partial Svg SolidFileImport();
+	//<iconPacks:PackIconBoxIcons Kind="SolidFileExport" />
+	public static partial Svg SolidFileExport();
+	//<iconPacks:PackIconMaterial Kind="DatabaseExport" />
+	public static partial Svg DatabaseExport();
+	//<iconPacks:PackIconMaterial Kind="DatabaseImport" />
+	public static partial Svg DatabaseImport();
+	//<iconPacks:PackIconLucide Kind="BookA" />
+	public static partial Svg BookA();
+
+	//<iconPacks:PackIconMaterialDesign Kind="Schema" />
+	public static partial Svg Schema();
+
+	//<iconPacks:PackIconMicrons Kind="VolHigh" />
+
+	public static partial Svg VolHigh();
+	//<iconPacks:PackIconIonicons Kind="CreateMD" />
+	public static partial Svg CreateMD();
+	//<iconPacks:PackIconMaterial Kind="DotsHorizontalCircleOutline" />
+	public static partial Svg DotsHorizontalCircleOutline();
+	//<iconPacks:PackIconFontisto Kind="MoreV" />
+	public static partial Svg MoreV();
+	//<iconPacks:PackIconMaterialDesign Kind="DeleteForeverSharp" />
+	public static partial Svg DeleteForeverSharp();
+
+	//public static partial Svg a;
+}
+
