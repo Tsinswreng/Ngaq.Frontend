201ebb5265e54eef204db2cc4e5990d04ec4945e
Tsinswreng <Tsinswreng@gmail.com>
2026-04-18 18:57:42 +0800

    改wasm入口

--- proj/Ngaq.Browser/Ngaq.Browser.cs
+++ proj/Ngaq.Browser/Ngaq.Browser.cs
@@ -8,24 +8,32 @@ using Ngaq.Ui;
 using Ngaq.Client;
 namespace Ngaq.Browser;
 internal sealed partial class Program {
-	private static Task Main(string[] args){
+	private static async Task Main(string[] args){
+		System.Console.WriteLine(114514);//t
+		try
+		{
+			var svc = new ServiceCollection();
+			svc
+				.SetupCore()
+				.SetupUi()
+				.SetupBrowser()
+				//.SetUpLocal()//TODO 改成按需API調用
+				.SetupClient()
+			;
+			var servicesProvider = svc.BuildServiceProvider();
 
-		var svc = new ServiceCollection();
-		svc
-			.SetupCore()
-			.SetupUi()
-			.SetupBrowser()
-			//.SetUpLocal()//TODO 改成按需API調用
-			.SetupClient()
-		;
-		var servicesProvider = svc.BuildServiceProvider();
-
-		return BuildAvaloniaApp()
-		.AfterSetup(e=>{
-			App.SetSvcProvider(servicesProvider);
-		})
-		.WithInterFont()
-		.StartBrowserAppAsync("out");//avalonia窗口 ʃʰ掛載ʹhtml元素
+			await BuildAvaloniaApp()
+			.AfterSetup(e=>{
+				App.SetSvcProvider(servicesProvider);
+			})
+			.WithInterFont()
+			.StartBrowserAppAsync("out");//avalonia窗口 ʃʰ掛載ʹhtml元素
+			return;
+			}
+		catch (System.Exception e){
+			System.Console.WriteLine(e);
+		}
+		return;
 
 	}
 
