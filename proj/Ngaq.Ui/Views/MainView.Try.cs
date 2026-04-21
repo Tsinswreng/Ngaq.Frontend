1f73e388cf26fe0c6a563a32eb95e741ec29fb29
Tsinswreng <Tsinswreng@gmail.com>
2026-04-21 21:39:04 +0800

    TextEditor json 語法高亮

--- proj/Ngaq.Ui/Views/MainView.Try.cs
+++ proj/Ngaq.Ui/Views/MainView.Try.cs
@@ -1,9 +1,11 @@
+using System.Text.RegularExpressions;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using AvaloniaEdit;
-using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.Rendering;
 
 namespace Ngaq.Ui.Views;
 
@@ -15,10 +17,10 @@ public partial class MainView {
 			FontFamily = new FontFamily("Consolas"),
 			FontSize = 14,
 			Foreground = Brushes.White,
-			SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("JSON"),
 			HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
 			VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
 		};
+		editor.TextArea.TextView.LineTransformers.Add(new JsonColorizer());
 		editor.Options.ConvertTabsToSpaces = true;
 		editor.Options.IndentationSize = 2;
 		editor.Options.EnableHyperlinks = false;
@@ -32,5 +34,28 @@ public partial class MainView {
 		};
 
 		window.Show();
+	}
+
+	sealed class JsonColorizer : DocumentColorizingTransformer {
+		static readonly Regex StringRegex = new("\"(?:\\\\.|[^\"\\\\])*\"", RegexOptions.Compiled);
+		static readonly Regex NumberRegex = new("-?(?:0|[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+-]?\\d+)?", RegexOptions.Compiled);
+		static readonly Regex BooleanRegex = new("\\btrue\\b|\\bfalse\\b", RegexOptions.Compiled);
+		static readonly Regex NullRegex = new("\\bnull\\b", RegexOptions.Compiled);
+
+		protected override void ColorizeLine(DocumentLine line) {
+			var text = CurrentContext.Document.GetText(line);
+			Colorize(line, text, StringRegex, Brushes.Orange);
+			Colorize(line, text, NumberRegex, Brushes.LightGreen);
+			Colorize(line, text, BooleanRegex, Brushes.DeepSkyBlue);
+			Colorize(line, text, NullRegex, Brushes.Gray);
+		}
+
+		void Colorize(DocumentLine line, string text, Regex regex, IBrush brush) {
+			foreach(Match match in regex.Matches(text)) {
+				var start = line.Offset + match.Index;
+				var end = start + match.Length;
+				ChangeLinePart(start, end, element => element.TextRunProperties.SetForegroundBrush(brush));
+			}
+		}
 	}
 }
