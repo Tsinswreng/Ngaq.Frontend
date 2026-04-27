215193df8b52685780a0e3b67e4a9c938f4a5e7a
Tsinswreng <Tsinswreng@gmail.com>
2026-04-27 20:53:05 +0800

    wip:編譯linux

--- proj/Ngaq.Linux/DiLinux.cs
+++ proj/Ngaq.Linux/DiLinux.cs
@@ -1,17 +1,14 @@
 using Microsoft.Extensions.DependencyInjection;
-using Ngaq.Core.Frontend.Hotkey;
 using Ngaq.Core.Shared.Audio;
 using Ngaq.Linux.Domains.Audio;
-using Ngaq.Linux.Domains.Hotkey;
+
 
 namespace Ngaq.Linux;
 
 public static class DiLinux{
 	public static IServiceCollection SetupLinux(this IServiceCollection z){
 		z.AddSingleton<IAudioPlayer, NAudioPlayer>();
-		z.AddSingleton<IHotkeyListener, WinHotkeyListener>();
 		// Linux 专用全局快捷键注册器
-		z.AddSingleton<I_RegisterGlobalHotKeys, WinGlobalHotkeyRegistrar>();
 		return z;
 	}
 }
